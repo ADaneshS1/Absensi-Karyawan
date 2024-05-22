0558c25d85e7c8fd1274bd653c9aead353623b03
Ahmad Danesh Sudiono <ahmaddaneshsudiono1.ppqita@gmail.com>
2024-05-22 13:54:27 +0700

    fix: add api data today

--- src/pages/api/getDataToday.js
+++ src/pages/api/getDataToday.js
@@ -1,13 +1,13 @@
 const { sql } = require("@vercel/postgres");
 
-async function getData(req,res) {
+export default async function getData(req,res) {
     try{
         if(req.method !== "GET") {
             return res.status(405).json({message:"Method tidak diperbolehkan"})
         }
 
-        const {rows} = await sql`SELECT * FROM absensi WHERE datediff(day,tanggal,now()) = 0`
+        const {rows} = await sql`SELECT * FROM absensi WHERE hari = ${(new Date()).getDate()} AND bulan =  ${(new Date()).getMonth() + 1} AND tahun =  ${(new Date()).getFullYear()}`
 
-        res.status
+        res.status(200).json({data:rows})
     }catch(e){}
 }
