316c4dc790761428a7c7fe921cb01098e5a38f62
Ahmad Danesh Sudiono <ahmaddaneshsudiono1.ppqita@gmail.com>
2024-05-23 14:01:31 +0700

    add: keterangan

--- src/pages/detail/[idDetail].js
+++ src/pages/detail/[idDetail].js
@@ -33,6 +33,8 @@ export default function DetailPage() {
                         {" "}
                         {data.jam_pulang}
                         {" "}
+                        {data.keterangan}
+                        {" "}
                         {data.hari}
                         {"/"}
                         {data.bulan}
